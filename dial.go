367ff8dbf27880c8902fbedbc9a8be1fd63ca631
daejin.lim <daejin.lim@navercorp.com>
2019-01-20 23:28:17 +0900

    init commit

---
+++ dial.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+
+	yaml "gopkg.in/yaml.v2"
+
+	ping "github.com/sparrc/go-ping"
+)
+
+type Config struct {
+	Type   string
+	Dst    string
+	Port   string
+	Notify string
+}
+
+type Configs struct {
+	Cfgs []Config `tasks`
+}
+
+func main() {
+	confFile, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Panicf("Read config. err #%v ", err)
+	}
+
+	var configs Configs
+	err = yaml.Unmarshal(confFile, &configs)
+	if err != nil {
+		log.Panicf("Parse config. err #%v ", err)
+	}
+
+	for _, config := range configs.Cfgs {
+		log.Println(config)
+		if config.Type == "ip" {
+			health, err := checkPort("tcp", config.Dst, config.Port)
+			log.Println(health, err)
+		}
+	}
+}
+
+func checkPort(protocol string, ip string, port string) (bool, error) {
+	conn, err := net.Dial(protocol, ip+":"+port)
+	if err != nil {
+		return false, err
+	} else {
+		defer conn.Close()
+		return true, nil
+	}
+}
+
+func checkPing() {
+	pinger, err := ping.NewPinger("10.64.53.130")
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.Println("success")
+	}
+
+	pinger.Count = 1
+	pinger.Run()
+}
+
+func checkDNSLookup() {
+
+}
+
+func notify() {
+
+}
